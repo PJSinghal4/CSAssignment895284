cd3ce71f86461625187e36832cf07b0df7fdd075
PJSinghal4 <135083079+PJSinghal4@users.noreply.github.com>
2024-02-14 23:41:48 -0500

    Add files via upload
    Assignment 2

---
+++ Assignment2/Interval.cs
@@ -0,0 +1,39 @@
+﻿
+class Interval
+{
+    static void Main()
+    {   //infinte loop so that we prompt user again and again until he types exit
+        while (true)
+        {
+            Console.WriteLine("Enter a real number (or type 'exit' to quit): ");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == "exit")
+            {//to break the loop
+                break;
+            }
+            //double input conversion
+            double x = Convert.ToDouble(userInput);
+
+            // Defining intervals using bool
+            bool interval1 = x >= 2 && x < 3;
+            bool interval2 = x > 0 && x <= 1;
+            bool interval3 = x >= -10 && x <= -2;
+
+            //using it as flag
+            bool inI = (interval1 || interval2) || interval3;
+
+            if (inI)
+            {
+                Console.WriteLine($"x belongs to I");
+            }
+            else
+            {
+                Console.WriteLine($"x does not belong to I");
+            }
+        }
+
+    }
+}
+
